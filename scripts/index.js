5a1b36ccf3e30b2d17d8e9b2cf39a1ade1234a50
hanna <rockyeaaah@gmail.com>
2022-12-06 17:18:58 +0200

    Solved mapping images and prices of products in shop.

--- scripts/index.js
+++ scripts/index.js
@@ -2,55 +2,65 @@
 
 //Shop section
 
-// const peaceIcon = document.getElementById("peace-solid-icon");
-
-// peaceIcon.src = '../images/peace-solid.svg';
-
-
-
-const productItemsImages = [
+const productItems = [
     {
         id: "health",
-        img: "../images/hospital-regular.svg"
+        img: "../images/hospital-regular.svg",
+        price: 5,
     },
     {
         id: "love",
-        img: "../images/heart-regular.svg"
+        img: "../images/heart-regular.svg",
+        price: 10,
     },
     {
         id: "time",
-        img: "../images/clock-regular.svg"
+        img: "../images/clock-regular.svg",
+        price: 8,
     },
     {
         id: "peace",
-        img: "../images/peace-solid.svg"
+        img: "../images/peace-solid.svg",
+        price: 4,
     },
 ];
 
+document.getElementById('cardContainer').innerHTML = productItems.map(shopCard =>
+    `<div class="skills-card flex-column">
+                <div class="imageItemBox">
+                    <img alt="product icon" id="${shopCard.id}" class="image-item" src="${shopCard.img}">
+                </div>
+                <h3>Health</h3>
+                <div class="price-box">
+                    <span>${shopCard.price}</span>
+                    <div><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div>
+                    <button class="buy-button">Buy</button>
+                </div>
+            </div>`
+).join('');
 
 
-    const productItemsElems = productItemsImages.map(element => {
-     /*   let elem = document.createElement("div");
-        elem.setAttribute("class", "product");*/
-        let productItem = `
-            <img alt="product icon" id="${element.id}" class="image-item" src="${element.img}"> `;
-        // elem.innerHTML = productItem;
-        return productItem;
-    });
-console.log(productItemsElems);
+/*
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-const imgItemBoxes = document.querySelectorAll("div.imageItemBox");
-console.log(imgItemBoxes);
-[...imgItemBoxes].map(element => {
-    element.innerHTML = productItemsElems.map(element => element);
-} );
+// Get the header
+var header = document.getElementById("myHeader");
 
-console.log(imgItemBoxes);
-/*    let container = document.querySelector(".product-list");
-    for (const product of productItems) {
-        container.append(product);
-    }*/
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+*/
 
 
 
