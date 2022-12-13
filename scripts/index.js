f6ef2e2769eb85f4cc3802286cbdfdd0ada9feb3
hanna <rockyeaaah@gmail.com>
2022-12-13 21:13:31 +0200

    Created logic of adding items to cart, without localstorage yet.

--- scripts/index.js
+++ scripts/index.js
@@ -1,114 +1,89 @@
-// Home section
-
+// SELECT ELEMENTS
+const authorizationButton = document.getElementById('authorization-button');
+const loginSuccessfulMessage = document.getElementById('login-successful');
+const loginWindow = document.getElementById('modal-login-window');
+const closeLoginWindowButton = document.getElementById('close-login-window-button');
+let cartButton = document.getElementById('cart-button');
+let shopCard = document.getElementById('cardContainer');
+const cartWindow = document.getElementById('modal-cart-window');
+const closeCartWindowButton = document.getElementById('close-cart-window-button');
+let loginForm = document.getElementById('login-form');
+let subtotalHeader = document.getElementById('subtotal');
+let numberOfItemsOnButton = document.getElementById('number-of-items-on-button');
 
 // Authorization Button
-const authorizationButton = document.getElementById('authorization-button');
 authorizationButton.innerHTML = 'Log in';
-const loginSuccessfulMessage = document.getElementById('login-successful');
 loginSuccessfulMessage.style.display = 'none';
 
-const loginWindow = document.getElementById('modal-login-window');
 
 function openLoginWindow() {
     loginWindow.style.display = 'block';
 }
 
 authorizationButton.addEventListener('click', openLoginWindow);
-const closeLoginWindowButton = document.getElementById('close-login-window-button');
 closeLoginWindowButton.addEventListener('click', (event) => {
     loginWindow.style.display = 'none';
 })
 
 
 // Shopping Cart Button
-let cartButton = document.getElementById('cart-button');
 // !!!!!!!!!!!! Поки розблочила
 // cartButton.disabled = true;
-
 
 
 //SHOP SECTION
 
 const productItems = [
     {
-        id: "health",
+        id: 0,
         img: "../images/hospital-regular.svg",
         price: 5,
         productCode: 'h',
+        name: "Health",
     },
     {
-        id: "love",
+        id: 1,
         img: "../images/heart-regular.svg",
         price: 10,
         productCode: 'l',
+        name: "Love",
     },
     {
-        id: "time",
+        id: 2,
         img: "../images/clock-regular.svg",
         price: 8,
         productCode: 't',
+        name: "Time",
     },
     {
-        id: "peace",
+        id: 3,
         img: "../images/peace-solid.svg",
         price: 4,
         productCode: 'p',
+        name: "Peace",
     },
 ];
 
-document.getElementById('cardContainer').innerHTML = productItems.map(shopCard =>
+shopCard.innerHTML = productItems.map(shopCard =>
     `<div class="skills-card flex-column">
                 <div class="imageItemBox">
-                    <img alt="product icon" id="${shopCard.id}" class="image-item" src="${shopCard.img}">
+                    <img alt="product icon"  class="image-item" src="${shopCard.img}">
                 </div>
-                <h3>Health</h3>
+                <h3>${shopCard.name}</h3>
                 <div class="price-box">
                     <span>${shopCard.price}</span>
                     <div><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div>
                     <button class="button buy-button" id="${shopCard.productCode}" 
-                    title=" Please, log in, if you want buy something.">Buy</button>
+                    title=" Please, log in, if you want buy something." onclick="addToCart(${shopCard.id})">Buy</button>
                 </div>
             </div>`
 ).join('');
 
 
-const cartWindow = document.getElementById('modal-cart-window');
+
 
 function openShoppingCartWindow() {
     cartWindow.style.display = 'block';
-    const productsInCart = localStorage.getItem('products');
-    const shoppingBox = document.getElementById('shopping-box');
-    // if else поміняй місцями
-    if (productsInCart) {
-        shoppingBox.innerHTML =
-        `<div class="no-items-header"><h4>No items yet</h4></div>`;
-    } else {
-
-        shoppingBox.innerHTML =  ` <div class="items-box" id="item-box"></div>
-<div class="checkout-button-block">
-            <button class="button" id="checkout-button">Checkout</button>
-        </div>`;
-        document.getElementById('item-box').innerHTML = productItems.map(shopCard =>
-           ` <div class="skills-card flex-column cart-product-card">
-<div class="imageItemBox">
-                    <img alt="product icon" id="${shopCard.id}" class="image-item" src="${shopCard.img}">
-                </div>
-                <h3>Health</h3>
-                <div class="price-box">
-                <div class="price-container">
-                    <span>${shopCard.price}</span>
-                    <div><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div>
-                    </div>
-                    <div class="counter-box">
-                    <button class="button minus-button counter-button" id="${shopCard.productCode}">-</button>
-                    <div class="counter">10</div>
-                      <button class="button plus-button counter-button">+</button>
-                      </div>
-        </div>
-    </div>
-        `
-    ).join('');
-    }
 }
 
 cartButton.addEventListener('click', openShoppingCartWindow);
@@ -117,8 +92,124 @@ function closeShoppingCartWindow() {
     cartWindow.style.display = 'none';
 }
 
-const closeCartWindowButton = document.getElementById('close-cart-window-button');
+
 closeCartWindowButton.addEventListener('click', closeShoppingCartWindow);
+
+
+// ADD TO CART
+let cart = [];
+
+function addToCart(id) {
+    // check if productItem already exist in cart
+    if (cart.some((item) => item.id === id)) {
+       changeNumberOfUnits('plus', id);
+    } else {
+        const item = productItems.find((product) => product.id === id);
+
+        cart.push({
+            ...item,
+            numberOfUnits: 1
+        });
+    }
+
+    updateCart();
+}
+
+// change items after adding in cart new one
+function updateCart() {
+    renderCartItems();
+    renderSubtotal();
+}
+// Calculate and render Subtotal
+function renderSubtotal() {
+    let totalPrice = 0, totalItems = 0;
+
+    cart.forEach((item) => {
+totalPrice += item.price * item.numberOfUnits;
+totalItems += item.numberOfUnits;
+    });
+
+    subtotalHeader.innerHTML = `
+    Subtotal (${totalItems} items): ${totalPrice}
+                        <div class="coin-subtotal-box">
+                            <img class="star-icon" alt="Star icon" src="images/star-solid.svg">
+                        </div>`;
+    numberOfItemsOnButton.innerHTML = `(${totalItems})`
+}
+
+// Render items in cart, if user buy something
+function renderCartItems() {
+    // Звяжись з localStorage!!!!!!!!
+    const productsInCart = localStorage.getItem('products');
+    const shoppingBox = document.getElementById('shopping-box');
+// if else поміняй місцями
+    if (productsInCart) {
+        // if there are some products, so render them
+        shoppingBox.innerHTML =
+            `<div class="no-items-header"><h4>No items yet</h4></div>`;
+        // console.log('test');
+    } else {
+
+        shoppingBox.innerHTML =
+            ` <div class="items-box" id="item-box">
+<!-- cart items will be here-->
+</div>`;
+
+        let cartElement = document.getElementById('item-box');
+        cartElement.innerHTML = ''; // clear cart element
+        cart.forEach((item) => {
+            cartElement.innerHTML +=
+                ` <div class="skills-card flex-column cart-product-card">
+<div class="imageItemCartBox">
+                    <img alt="product icon" class="image-item" src="${item.img}" alt="${item.name}">
+                </div>
+                <h3>${item.name}</h3>
+                <div class="price-box">
+                <div class="price-container">
+                    <span>${item.price}</span>
+                    <div><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div>
+                    </div>
+                    <div class="counter-box">
+                    <button class="button minus-button counter-button" onclick="changeNumberOfUnits('minus', ${item.id})">-</button>
+                    <div class="counter">${item.numberOfUnits}</div>
+                      <button class="button plus-button counter-button" onclick="changeNumberOfUnits('plus', ${item.id})">+</button>
+                      </div>
+        </div>
+             <button class="button" onclick="removeItemFromCart(${item.id})">Delete</button>
+    </div>
+        `;
+        })
+    }
+}
+
+// Remove item from cart
+function removeItemFromCart(id) {
+    cart = cart.filter((item) => item.id !== id);
+    updateCart();
+}
+
+// Change number of units in cart for each item
+function changeNumberOfUnits(action, id) {
+    cart = cart.map((item) => {
+        let numberOfUnits = item.numberOfUnits;
+
+        if (item.id === id) {
+            if(action === 'minus' && numberOfUnits > 0) {
+                numberOfUnits--;
+            } else if(action === 'plus') {
+                numberOfUnits++;
+            }
+        }
+
+        return {
+            ...item,
+            numberOfUnits,
+        };
+    })
+
+    updateCart();
+}
+
 
 // Login
 function storeLoginData() {
@@ -159,7 +250,7 @@ function checkLoginData(event) {
 }
 
 
-let loginForm = document.getElementById('login-form');
+
 loginForm.addEventListener('submit', checkLoginData);
 
 
@@ -195,15 +286,46 @@ function enableBuyButton() {
 
 authorizationButton.addEventListener('click', enableShoppingCartButton);*/
 
-/*let cartItems = [];
-const addToCartHandler = (id) => {
-    productItems.map((item) => {
-        if(item.id === id) {
-let savedItems = [...cartItems, item];
-localStorage.setItem('cartItems', JSON.stringify(savedItems));
-        }
-        return item;
-    });
-};
 
-buyButton.addEventListener('click', addToCartHandler);*/
+//навсяк скопіювала вміст рендерингу карток мокапом з  function closeShoppingCartWindow()  , поки пишу справжню функцію. Потім цей код можна буде стерти
+
+/*
+const productsInCart = localStorage.getItem('products');
+const shoppingBox = document.getElementById('shopping-box');
+// if else поміняй місцями
+if (productsInCart) {
+    // if there are some products, so render them
+    shoppingBox.innerHTML =
+        `<div class="no-items-header"><h4>No items yet</h4></div>`;
+} else {
+
+    shoppingBox.innerHTML =
+        ` <div class="items-box" id="item-box"></div>
+<div class="checkout-button-block">
+<h4>Subtotal (0 items): 0 <div class="coin-subtotal-box"><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div></h4>
+            <button class="button" id="checkout-button">Checkout</button>
+        </div>`;
+
+    document.getElementById('item-box').innerHTML = productItems.map(shopCard =>
+        ` <div class="skills-card flex-column cart-product-card">
+<div class="imageItemCartBox">
+                    <img alt="product icon" id="${shopCard.id}" class="image-item" src="${shopCard.img}">
+                </div>
+                <h3>Health</h3>
+                <div class="price-box">
+                <div class="price-container">
+                    <span>${shopCard.price}</span>
+                    <div><img class="star-icon" alt="Star icon" src="images/star-solid.svg"></div>
+                    </div>
+                    <div class="counter-box">
+                    <button class="button minus-button counter-button" id="${shopCard.productCode}">-</button>
+                    <div class="counter">10</div>
+                      <button class="button plus-button counter-button">+</button>
+                      </div>
+        </div>
+    </div>
+        `
+    ).join('');
+
+
+}*/
